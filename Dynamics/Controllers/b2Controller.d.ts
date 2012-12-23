0b55edcdc3bdec042af8e0f10af8ee3e7417022c
jbaldwin <joshrbaldwin@gmail.com>
2012-12-23 10:48:29 -0700

    Updated b2Controller.d.ts with comments
    Unsure on Step(b2TimeStep), appears to be a private internal class but
    it is referenced publically here.

--- Dynamics/Controllers/b2Controller.d.ts
+++ Dynamics/Controllers/b2Controller.d.ts
@@ -5,18 +5,66 @@
 
 module Box2D.Dynamics.Controllers {
 
+	/**
+	* Base class for controllers. Controllers are a convience for encapsulating common per-step functionality.
+	**/
 	export class b2Controller {
+		
+		/**
+		* Body count.
+		**/
 		public m_bodyCount: number;
+
+		/**
+		* List of bodies.
+		**/
 		public m_bodyList: b2ControllerEdge;
 
-		constructor();
+		/**
+		* Adds a body to the controller.
+		* @body Body to add.
+		**/
 		public AddBody(body: b2Body): void;
+
+		/**
+		* Removes all bodies from the controller.
+		**/
 		public Clear(): void;
+
+		/**
+		* Debug drawing.
+		* @debugDraw Handle to drawer.
+		**/
 		public Draw(debugDraw: b2DebugDraw): void;
+
+		/**
+		* Gets the body list.
+		* @return Body list.
+		**/
 		public GetBodyList(): b2ControllerEdge;
+
+		/**
+		* Gets the next controller.
+		* @return Next controller.
+		**/
 		public GetNext(): b2Controller;
+
+		/**
+		* Gets the world.
+		* @return World.
+		**/
 		public GetWorld(): b2World;
+
+		/**
+		* Removes a body from the controller.
+		* @body Body to remove from this controller.
+		**/
 		public RemoveBody(body: b2Body): void;
-		public Step(step: any): void;
+
+		/**
+		* Step
+		* @step b2TimeStep -> Private internal class.
+		**/
+		public Step(step: any/*b2TimeStep*/): void;
 	}
 }
