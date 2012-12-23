c3fd15b455b71cdc4749eef706d8ff4fba190786
jbaldwin <joshrbaldwin@gmail.com>
2012-12-23 13:27:51 -0700

    Updated b2JointDef with comments

--- Dynamics/Joints/b2JointDef.d.ts
+++ Dynamics/Joints/b2JointDef.d.ts
@@ -2,13 +2,39 @@
 
 module Box2D.Dynamics.Joints {
 	
+	/**
+	* Joint definitions are used to construct joints.
+	**/
 	export class b2JointDef {
+
+		/**
+		* The first attached body.
+		**/
 		public bodyA: b2Body;
+
+		/**
+		* The second attached body.
+		**/
 		public bodyB: b2Body;
+
+		/**
+		* Set this flag to true if the attached bodies should collide.
+		**/
 		public collideConnected: bool;
+
+		/**
+		* The joint type is set automatically for concrete joint types.
+		**/
 		public type: number;
+
+		/**
+		* Use this to attach application specific data to your joints.
+		**/
 		public userData: any;
 
+		/**
+		* Constructor.
+		**/
 		constructor();
 	}
 }
