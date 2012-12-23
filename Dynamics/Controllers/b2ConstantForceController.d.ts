ed36ead7d0e5e453bfff8ce7e241e1b0185d12d2
jbaldwin <joshrbaldwin@gmail.com>
2012-12-23 13:13:30 -0700

    Added b2ConstantAccelController.d.ts definitions

---
+++ Dynamics/Controllers/b2ConstantForceController.d.ts
@@ -0,0 +1,23 @@
+/// <reference path="../../Common/Math/init.d.ts" />
+/// <reference path="../../Common/Math/b2Vec2.d.ts" />
+/// <reference path="b2Controller.d.ts" />
+
+module Box2D.Dynamics.Controllers {
+
+	/**
+	* Applies an acceleration every frame, like gravity.
+	**/
+	export class b2ConstantForceController extends b2Controller {
+
+		/**
+		* The acceleration to apply.
+		**/
+		public A: b2Math.b2Vec2;
+
+		/**
+		* Step.
+		* @step Internal b2TimeStep structure.
+		**/
+		public Step(step: any/* b2TimeStep*/): void;
+	}
+}
