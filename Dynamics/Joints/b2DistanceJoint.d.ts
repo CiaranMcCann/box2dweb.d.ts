b198e0657154ed022797f5bf8594e88d3b0d851e
jbaldwin <joshrbaldwin@gmail.com>
2012-12-24 10:34:58 -0700

    Added b2DistanceJoint.d.ts definitions

---
+++ Dynamics/Joints/b2DistanceJoint.d.ts
@@ -0,0 +1,73 @@
+/// <reference path="../../Common/Math/init.d.ts" />
+/// <reference path="../../Common/Math/b2Vec2.d.ts" />
+/// <reference path="b2Joint.d.ts" />
+
+module Box2D.Dynamics.Joints {
+
+	/**
+	* A distance joint constrains two points on two bodies to remain at a fixed distance from each other. You can view this as a massless, rigid rod.
+	**/
+	export class b2DistanceJoint extends b2Joint {
+
+		/**
+		* Get the anchor point on bodyA in world coordinates.
+		* @return Body A anchor.
+		**/
+		public GetAnchorA(): b2Math.b2Vec2;
+
+		/**
+		* Get the anchor point on bodyB in world coordinates.
+		* @return Body B anchor.
+		**/
+		public GetAnchorB(): b2Math.b2Vec2;
+
+		/**
+		* Gets the damping ratio.
+		* @return Damping ratio.
+		**/
+		public GetDampingRatio(): number;
+
+		/**
+		* Gets the frequency.
+		* @return Frequency.
+		**/
+		public GetFrequency(): number;
+
+		/**
+		* Gets the length of distance between the two bodies.
+		* @return Length.
+		**/
+		public GetLength(): number;
+
+		/**
+		* Get the reaction force on body2 at the joint anchor in N.
+		* @inv_dt
+		* @return Reaction force in N.
+		**/
+		public GetReactionForce(inv_dt: number); b2Math.b2Vec2;
+
+		/**
+		* Get the reaction torque on body 2 in N.
+		* @return Reaction torque in N.
+		**/
+		public GetReactionTorque(inv_dt: number); number;
+
+		/**
+		* Sets the damping ratio.
+		* @ratio New damping ratio.
+		**/
+		public SetDampingRatio(ratio: number): void;
+
+		/**
+		* Sets the frequency.
+		* @hz New frequency (hertz).
+		**/
+		public SetFrequency(hz: number): void;
+
+		/**
+		* Sets the length of distance between the two bodies.
+		* @length New length.
+		**/
+		public SetLength(length: number): void;
+	}
+}
